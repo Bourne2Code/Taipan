ce696da8cd04573a24df6fc79a8f0ab4c6974f66
Robert <RobertDBourne@gmail.com>
2022-06-07 18:23:19 -0600

    reorder

--- src/Trader.py
+++ src/Trader.py
@@ -3,25 +3,26 @@ import random
 #from colorama import Fore, Back, Style
 
 global Company_Name
-global TradedItems_Name
+global Port_Names               # Constant? 
+global TradedItems_Name         # Constant? 
 global TradedItems_Price
 global TradedItems_InShip
 global TradedItems_InWarehouse
 global Silver_OnHand
 global Silver_InBank
 global Ship_Capacity
+global Current_Port
 
 global User_Action
 
+
+Port_Names = ["Hong Kong", "Shanghai", "Nagasaki", "Saigon", "Manila", "Singapore", "Batavia"]
 TradedItems_Name = ["Silk", "Tea", "Gunpowder", "Opium"]
 TradedItems_Price = [20, 10, 50, 500]
+Current_Port = 0
 
 def Clear_Screen():
     os.system('cls')
-
-def Set_Prices():
-    global TradedItems_Price
-    TradedItems_Price = [random.randrange(5, 45, 1), random.randrange(2, 25, 1), random.randrange(5, 55, 1), random.randrange(300, 999, 1)]
 
 def Config_Player():
     global Company_Name
@@ -40,7 +41,54 @@ def Config_Player():
     TradedItems_InShip = [0, 0, 0, 0]
     TradedItems_InWarehouse = [0, 0, 0, 0]
 
+def Show_Status():
+    global TradedItems_Name
+    global TradedItems_Price
+    global Silver_OnHand
+    global Ship_Capacity
+    global TradedItems_InShip
 
+    Clear_Screen()
+    Game_Banner = Company_Name + " in the port of " + Port_Names[Current_Port]
+    
+    print(Game_Banner.center(80)+"\n")
+    print('\033[32m' + "CURRENT PRICES")
+    for i in range(4):
+        print(TradedItems_Name[i].ljust(10) + ": " + str(TradedItems_Price[i]).rjust(3))
+
+    print('\033[32m' + "\nSHIP CARGO")
+    for i in range(4):
+        print(TradedItems_Name[i].ljust(10) + ": " + str(TradedItems_InShip[i]).rjust(3))
+
+
+    FreeSpace_InShip = int(Ship_Capacity - TradedItems_InShip[0] - TradedItems_InShip[1] - TradedItems_InShip[2] - TradedItems_InShip[3])
+    print(f"Availabile space in ship: {FreeSpace_InShip} of {Ship_Capacity}")
+    print('\033[39m')
+    print(f"Silver on hand: {Silver_OnHand}")
+
+
+def Set_Prices():
+    global TradedItems_Price
+    TradedItems_Price = [random.randrange(5, 45, 1), random.randrange(2, 25, 1), random.randrange(5, 55, 1), random.randrange(300, 999, 1)]
+
+def Buy_Cargo(tI):
+    global TradedItems_Name
+    global TradedItems_Price
+    global Silver_OnHand
+    global Ship_Capacity
+    global TradedItems_InShip
+
+    print(f"Buy {TradedItems_Name[tI]}!")
+    Can_Buy = Silver_OnHand // TradedItems_Price[tI]
+    print(f"You can afford {Can_Buy} {TradedItems_Name[tI]}.")
+    Want_Buy = int(input("How much do you want to buy?"))
+    if (Want_Buy > Can_Buy):
+        print("You cannot afford that much!")
+    elif (Want_Buy > Ship_Capacity):
+        print("Your ship cannot hold that much!")
+    else:
+        Silver_OnHand = (Silver_OnHand - Want_Buy * TradedItems_Price[tI])
+        TradedItems_InShip[tI] = (TradedItems_InShip[tI] + Want_Buy)
 
 def Buy_SelectItem():
     global TradedItems_Name
@@ -62,46 +110,6 @@ def Buy_SelectItem():
         case _:
             print("What?")
 
-
-def Buy_Cargo(tI):
-    global TradedItems_Name
-    global TradedItems_Price
-    global Silver_OnHand
-    global Ship_Capacity
-    global TradedItems_InShip
-
-    print(f"Buy {TradedItems_Name[tI]}!")
-    Can_Buy = Silver_OnHand // TradedItems_Price[tI]
-    print(f"You can afford {Can_Buy} {TradedItems_Name[tI]}.")
-    Want_Buy = int(input("How much do you want to buy?"))
-    if (Want_Buy > Can_Buy):
-        print("You cannot afford that much!")
-    elif (Want_Buy > Ship_Capacity):
-        print("Your ship cannot hold that much!")
-    else:
-        Ship_Capacity = (Ship_Capacity - Want_Buy)
-        Silver_OnHand = (Silver_OnHand - Want_Buy * TradedItems_Price[tI])
-        TradedItems_InShip[tI] = (TradedItems_InShip[tI] + Want_Buy)
-           
-def Show_Status():
-    global TradedItems_Name
-    global TradedItems_Price
-    global Silver_OnHand
-    global Ship_Capacity
-    global TradedItems_InShip
-
-    Clear_Screen()
-    print(Company_Name.center(80)+"\n")
-    print('\033[32m' + "CURRENT PRICES")
-    for i in range(4):
-        print(TradedItems_Name[i].ljust(10) + ": " + str(TradedItems_Price[i]).rjust(3))
-
-    print('\033[32m' + "\nSHIP CARGO")
-    for i in range(4):
-        print(TradedItems_Name[i].ljust(10) + ": " + str(TradedItems_InShip[i]).rjust(3))
-
-    print('\033[39m')
-    print(f"Silver on hand: {Silver_OnHand}")
 
 
 
